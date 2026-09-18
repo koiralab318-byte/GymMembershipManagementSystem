3608a5941068bb8201ac4a0405a9a71ed3d19d87
Bishal Koirala <koiralab318@gmail.com>
2026-09-18 22:19:48 +1000

    Created Membership class

---
+++ GymMembershipManagementSystem/Membership.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymMembershipManagementSystem
+{
+    public class Membership
+    {
+        public int MembershipID { get; set; }
+
+        public int MemberID { get; set; }
+
+        public string MembershipType { get; set; } = "";
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime ExpiryDate { get; set; }
+
+        public decimal Fee { get; set; }
+
+        public string PaymentStatus { get; set; } = "";
+
+        public string MembershipStatus { get; set; } = "";
+    }
+}
